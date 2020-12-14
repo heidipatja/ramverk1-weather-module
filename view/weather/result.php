7d75235cbd28d458b7a6d1847268f5c1f70c402d
Heidi Patja <hpatja@gmail.com>
2020-12-14 15:58:48 +0100

    View fix for testing

--- view/weather/result.php
+++ view/weather/result.php
@@ -9,7 +9,7 @@ namespace Anax\View;
 // var_dump($weather);
 
 ?>
-    <?php if (is_object($weather)) : ?>
+    <?php if (isset($weather[0]["date"])) : ?>
         <h3>Väder</h3>
         <div class="weather columns col3-wrapper">
             <?php foreach ($weather as $day) : ?>
